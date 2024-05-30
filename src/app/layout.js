70db8d827feb94a50c05b3094f89eb82d115fc36
Rajkumar <110892356+rkpymb@users.noreply.github.com>
2024-05-30 20:49:07 +0530

    dfsdsd

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
                 <div className={styles.ChatSectionA}>
                   <AutoLogin />
                   <AsideTop />
-                  {/* <CreateGroup /> */}
+                  <CreateGroup />
                   <MyGroups />
                  
                   {/* <GroupLists /> */}
